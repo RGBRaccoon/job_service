52541a023e16ac9b51440740c54208a1561b17b1
RGB_Raccoon <chldusgh0497@gmail.com>
2024-11-28 12:53:21 +0900

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from config.fastapi_config import CustomGunicornApp
+from api.router.test_router import total_test_router
+from api.router.router import api_router
+
+# FastAPI 애플리케이션 정의
+
+
+app = FastAPI()
+# app.include_router(router=total_test_router)
+app.include_router(router=api_router)
+if __name__ == "__main__":
+    # Gunicorn 서버를 Python 코드 내에서 실행
+    CustomGunicornApp(app=app).run()
---
+++ config/fastapi_config.py
@@ -0,0 +1,24 @@
+from gunicorn.app.base import BaseApplication
+
+from config.db_config import create_db
+
+
+class CustomGunicornApp(BaseApplication):
+    def __init__(self, app):
+        self.app = app
+        self.options = {
+            "bind": "0.0.0.0:8000",  # 서버 바인딩 주소 및 포트
+            "workers": 4,  # 워커 수
+            "worker_class": "uvicorn.workers.UvicornWorker",  # Uvicorn ASGI 워커
+            "loglevel": "info",  # 로그 레벨 설정
+        }
+        create_db()
+        super().__init__()
+
+    def load_config(self):
+        config = {key: value for key, value in self.options.items() if key in self.cfg.settings and value is not None}
+        for key, value in config.items():
+            self.cfg.set(key.lower(), value)
+
+    def load(self):
+        return self.app
