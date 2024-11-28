7dc2f4955bb2619c5c0bdde84d17c1fa1d532abe
RGB_Raccoon <chldusgh0497@gmail.com>
2024-11-28 19:20:47 +0900

    기본 모델 추가 및 스키마 작업 등. 크롤링은 일시적으로 보류->api가 존재->문의 필요

---
+++ model/job_post_model.py
@@ -0,0 +1,6 @@
+from config.db_config import Base
+
+
+class JobPostModel(Base):
+    __tablename__ = "job_post"
+    pass
---
+++ service/crwaler.py
@@ -0,0 +1,7 @@
+class Crawler:
+
+    def get_enterprize_data(self):
+        pass
+
+    def get_recruit_data(self):
+        pass
