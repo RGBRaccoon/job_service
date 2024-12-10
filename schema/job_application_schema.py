6d7aceee4954e114685062301770e1d746a327bd
RGB_Raccoon <chldusgh0497@gmail.com>
2024-12-10 14:26:54 +0900

    스키마 버그 수정

--- schema/job_application_schema.py
+++ schema/job_application_schema.py
@@ -1,15 +1,14 @@
+from typing import Optional
 from uuid import UUID
 from pydantic import BaseModel, ConfigDict
 
 
 class JobApplication(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    pass
 
 
 class JobApplicationCreate(JobApplication):
-    user_id: UUID
-    pass
+    user_id: Optional[UUID] = None
 
 
 class JobApplicationResponse(JobApplication):
