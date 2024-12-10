d8939587d4ccf58c7981f26c49117c5b0f1680ea
RGBRaccoon <chldusgh0497@gmail.com>
2024-12-11 08:02:44 +0900

    설정 jcloud에 맞추어 수정

--- config/fastapi_config.py
+++ config/fastapi_config.py
@@ -7,7 +7,7 @@ class CustomGunicornApp(BaseApplication):
     def __init__(self, app):
         self.app = app
         self.options = {
-            "bind": "0.0.0.0:8000",  # 서버 바인딩 주소 및 포트
+            "bind": "0.0.0.0:8080",  # 서버 바인딩 주소 및 포트
             "workers": 4,  # 워커 수
             "worker_class": "uvicorn.workers.UvicornWorker",  # Uvicorn ASGI 워커
             "loglevel": "info",  # 로그 레벨 설정
